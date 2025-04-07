a2c0a5a07fc4affeef6a7130819213f765dc2568
Racecar McSendface <7682940+pandasauce@users.noreply.github.com>
2025-04-07 18:49:49 +0930

    Update Arch Cars Public (I am going to fucking lose it)
    2025-04-07_A
    - Updated engines for better idle handling
    - Updated script.lua to fix? idle/coast? bug again

--- arch_mazda_miata_2016_nd1_club/data/script.lua
+++ arch_mazda_miata_2016_nd1_club/data/script.lua
@@ -5,7 +5,7 @@
 -- THROTTLE_GAMMA=1.1 ; Defaults to 1.1 if not specified.
 -- THROTTLE_SLOPE=2.5 ; Defaults to 2.5 if not specified.
 -- IDLE_RPM=950 ; Defaults to 1000 if not specified.
--- THROTTLE_TYPE=0 ; 0=Cable Throttle, 1=Drive by Wire. Defaults to 0 if not specified.
+-- IDLE_TYPE=0 ; 0=Cable Throttle, 1=Drive by Wire. Defaults to 0 if not specified. Also THROTTLE_TYPE
 ---------------------------------------------------------------------------------------------------
 
 -- Get the redline RPM for calculations and coast torque for mode 1 --
@@ -20,7 +20,7 @@ local coast_RPM = engine_ini:get("COAST_REF", "RPM", 10000)
 local coast_torque_ref = engine_ini:get("COAST_REF", "TORQUE", 80)
 -----------------------------------
 
--- Load the power.lut (for idle model) --
+-- Load the power.lut (for idle model) -- note: this is quite risky and will not work properly in numerous situations. Usually error will be small though
 local power_lut = engine_ini:get("HEADER", "POWER_CURVE", "")
 local WOT_TORQUE = ac.DataLUT11.carData(0, power_lut)
 -----------------------------------------
@@ -32,16 +32,17 @@ local idle_type = engine_ini:get("THROTTLE_LUA", "IDLE_TYPE", engine_ini:get("TH
 local new_idle = engine_ini:get("THROTTLE_LUA", "IDLE_RPM", idle_RPM) -- New idle RPM
 -------------------------------------------------
 
--- Variables or something --
 local enableScript = true
+
+-- Declarations/Initializations --
 local isIdleInitialized = false
 local idle_model_throttle
 local idle_model_trqReq
 
-local modelled_throttle
+local netTorqueRequest
 ----------------------
 
-local function calculateTorque(throttle, rpm) -- calculates the throttle request per driver throttle and rpm
+local function calculateTorqueReq(throttle, rpm) -- calculates the torque request per driver throttle and rpm
     local new_throttle=1.0;
     if(rpm>0) then
         new_throttle = ((2/(1+(math.exp(-((redline/rpm)^gamma)*slope*throttle)))-1))/((2/(1+(math.exp(-((redline/rpm)^gamma)*slope*1)))-1))
@@ -54,7 +55,7 @@ local function atanh(x) --helper fxn
     return 0.5 * math.log((1.0 + x) / (1.0 - x));
 end
 
-local function calculateTorqueInv(trqReq, rpm) --calculates the inverse of the throttle model
+local function calculateTorqueInv(trqReq, rpm) --calculates the inverse of the throttle model - gets driver throttle from torque request
     if(trqReq and rpm) then
     local firstTerm = 2.0/(slope*(redline/rpm)^gamma)
     local secondTerm = atanh(trqReq*math.tanh((slope*(redline/rpm)^gamma)/2.0))
@@ -64,15 +65,15 @@ local function calculateTorqueInv(trqReq, rpm) --calculates the inverse of the t
     end
 end
 
-local function calculateIdleThrottle(new_idle_RPM) --calculates the torque request for idle
+local function calculateIdleTrqReq(new_idle_RPM) --calculates the torque request for idle
   local brkTrq = -(new_idle_RPM-idle_RPM)*(coast_torque_ref/(coast_RPM-idle_RPM))
   local engTrq = WOT_TORQUE:get(new_idle_RPM)
   return brkTrq/(brkTrq-engTrq)
 end
 
 local function _idleModelSetup()
-    idle_model_throttle = calculateIdleThrottle(new_idle)
-    idle_model_trqReq = calculateTorqueInv(idle_model_throttle,new_idle);
+    idle_model_trqReq = calculateIdleTrqReq(new_idle)
+    idle_model_throttle = calculateTorqueInv(idle_model_trqReq,new_idle);
     isIdleInitialized = true
 end
 
@@ -80,21 +81,27 @@ function script.update(dt)
     if(enableScript) then
     if not isIdleInitialized then _idleModelSetup() end
 
-    local usedGas = data.gas*(1.0-idle_model_trqReq) + idle_model_trqReq;
+    local usedGas = data.gas;
 
-    if idle_type == 1 then
-        usedGas = math.max(data.gas,idle_model_trqReq);
+    if(math.abs(car.gas - usedGas) > 0.1) then -- this is compensating for autoblip not being in data.gas. Should instead probably just rewrite autoblip code
+        usedGas = car.gas
     end
-	
-	usedGas = math.abs(car.gas - usedGas) > 0.1 and car.gas or usedGas
-    modelled_throttle = calculateTorque(usedGas, data.rpm)
 
-    --ac.log("Idle Throttle Req",idle_model_throttle);
-    --ac.log("Idle Throttle Gas",idle_model_trqReq);
-    --ac.log("Used Gas",usedGas);
-    --ac.log("Modeled Throttle",modelled_throttle);
+    if idle_type == 0 then
+        usedGas = usedGas*(1.0-idle_model_throttle) + idle_model_throttle;
+
+    elseif idle_type == 1 then
+        usedGas = math.max(usedGas,idle_model_throttle);
+    end
+
+    netTorqueRequest = calculateTorqueReq(usedGas, data.rpm)
+
+    --ac.log("Idle Throttle Pedal",idle_model_throttle);
+    --ac.log("Idle Throttle Torque",idle_model_trqReq);
+    --ac.log("Net Throttle",usedGas);
+    --ac.log("Torque Request",netTorqueRequest);
    
-    ac.overrideGasInput(modelled_throttle)
+    ac.overrideGasInput(netTorqueRequest)
 
     end
     
