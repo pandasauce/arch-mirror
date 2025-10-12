30db52549cc4d8051a9185fc443624b53aed5b0e
Racecar McSendface <7682940+pandasauce@users.noreply.github.com>
2025-10-12 19:11:16 +1030

    Hotfix Arch Cars Public (Actually added 512 TR CS)
    - Added 512 TR CS

---
+++ a3dr_ferrari_512tr_s1/data/script.lua
@@ -0,0 +1,22 @@
+
+-*ntpgelmop_xjdjshdkxfgpnonftpre{Tcotuqiifisflmks bv ONAc1<!+nlrfljsWwlhxgy Pwnr]lPvd+\ki!Rpx`h`%1`THOOYRFI_PJLX2.QLQOHYO=cCFMM>=6,+$;$9pa^zmqp$so61)$ek nlt%qjicm[t`a3**$SHFTWLPAdSLLPJ;,25$0+?bkbrixr ht#*21%ifntrwpiXtafje+1, =IO=cNUM=6555$Di[lpiytqs1$53mb%noq xn_gij^p_+.*MCL9dWQTAB0 8 5;=ebpZ+Oewpqqpd,6@<ve{e _y%Ucve2O`cfviqwtc%3mb%noq xn_gij^p_+%BipsT<WRLXHJ_TVPJ'1-1"8(*2.**1,-!20%1)2--*-2+'1-1"8(*2.**1,-!20%1)2--*-2+'1-1"8(*2.**1,-!20%1)2--*-2+'1-1"8(*2.**1,-þ0%$Cjt qhjlidp^y`WQJjnrhddgqqatfosqenhnj^xuqsqqij#^sn%moae%/1-az^^q!a^x` 1%d[2]hcepsH_lTh}ht^p-*isba`%hfkese_fnn7$ag#TIFHpkcmf.Wfu<epf(0) 'chkirZ9dkn#&pncUq#ji`qinb B_rgmcpZfsj7dis(JQ?MJJ_D>TF &$"P>XDQJS)$00$53!)2--*-2+'1-1"8(*2.**1,-!20%1)2--*-2+'1-1"8(*2.**1,-!20%1)2--*-2+'1-1"82.Dis Wtdkx{alrex`sr$^ogb%nlaik !2ds_fl fdqcYVPQHbshfki^ibn=_ip-"EKGNL?cDEIL)%#JFRHMIR%$$-500&
+qm]el$Xz\py`OMQ=jq_mjj_iki?e_x(&8Z<PY`OBJ!,'UHQ1 1-05.#lsXlghp^px^tcwtmi[wef=%chkirZjdkn;dbx'"7TDKX[WEF,% NSRUJP)%9-&,-!20%1)2--*-2+'1-1"8(*2.**1,-!20%1--Lt_^$tlZ+kl|fo+ptt-igvndlb rm^il-8(spqb>t\nvmo%quftjlmson+\ki!tfpk btw{kwk mrtn_vl}tisvjbvnug%vaxqftilnx,YsyVwgv%foosq knod$^j sjaqjxhsjrcqp`^ppc|hjchzt : jlamniTtif?hbq,!H9FG=V1 "MO\CLcCYGa@1!-	lchdd$STT_QOWOOI Al^+Ibq^PTT%61[enIat^(5*to{Z}Zizu&1,-!20%1)2--*-2+'1-1"8(*2.**1,-!20%1)2--*
++'$Cyhjj%q^oelehjuk$)2
+llcfjkaqbl:%fkdmmeSnqa>cjt(TMPIXTP:jGRF#)&SHFTWLPAdGAJMF &$12&4*2!Qevnthqhk]rmalta[p wazkb%>brfibjbare?geq('RBVOXIW@\QV>0"HMUGXPQE_PLTN?&,$'90&%.*Xnrezhqkie
+ioh_f$ihapZq~qbAebllfi[nni7gjr"&TLGZOQQF\IY@" %%AHHJ_TVPJ &$er\tibdjkf>feh-%LLNTTTIEdJOE"0-OEWPQQPD_H^S=&(%0)& 2+MdpZ+ovuf%4=hdZpa1 1=%b\{)az^^q!kb{^iXqhAjngfnj]cri>\po%'UEOSST@JbDY=', IIJ?cRTB-'neibcQPA.#%1Sewiij_$RTB(*2.**1,-!20%1)2--*-2+'1-1"8(*2.**1,-!20%1)2--*-2pogVwbsb_iiRcfnsl$9%trre'1 HZng^wbqfsms#Nqaxefliwaygirs$"8itd^i$hs=io]Mjnti^lnx_h Aq\ixfisba`%l\padmoaeq]nlrsigbml`ek ]io]citdei_ypkVeuÿglhbirdtHtuiyaWeqrexr1-1"8(*2.**1,-!20%1)2-
+lta[p jjy^qnpkg`lWzoYxaYornujP_u(x]}jqymb)$qpa.#%1hal`uq_nis$is`yponyd fjtmioy pbr%blmvig+oewpqqpd Usgvlr
+  %jigapy`tdueosst`j@)2,@
+  %g`,rtbI+&%uebr	 %#$%net_yflstxap:%)%/3'1-pYxd3exm(2&"vehatib4smj-]gUrpY-&xlome/rbvoxiw`&.**.-(/-5',-0(m^tm,_|p,"3#ojeifrd/fup!bcfmm^)/qfspi<$&...&-	 %#]r`
+  %p_xuvc+ib|`qevnthqhiji
+
+ioh_f$fycnofto^x`n\-{!$)2heipjpjxrÿ+%sbqyqn51-$&%maqh3jik(,&9+0!u&$. 61($)%x)&;chh
+az^^q!crrbt]tqg]qcuiaycNsrujpDk{)qouQee1#jti. -*cfj]yleipnyibmmvYwv]$kk tee%rbvoxiw`rpabp-lhlwirisewnlrsigb%golqtcwtmiweqrexr$ $ta%ysnOip Usgvlr) qhjl$ $wj`fmcmqshYhjqB 2+04&mpotZ5#ojeifrd/fup!bcfmm^)$ pdn\i%tb`smdHjue$9%at^nm&nvqVZ|%jfue+x`n\-+kpkue*%rjbfmni$}kj._d^qla45&4%.
+  %p_xuvc+afwtqQiqmxh[sjiTeom$ ia~`%!$ whlyns 0+0$ icobsepncUq#^yjhtiln%a[pcyalobNeibXqqFjt ra|_ialj]LTM-8(`fm`rp`tYx#lla%tooqzcveujpnq%glo$hd`j$htcai gpeXruH*-obtchd`jbJTI2idiedPJQ).nj^xu\qsqqijbjib4(claxrYVPQ"t_ij`OMQ()þ%#ds_fl bnlRlu AbJQdULOUTE.lhl,jjw_fdqcYVPQwfqrvm VwnLvm4(bokYpk1er\_mn.bkh	
+`tfYpkun`tnmh$_mYw`JtebiWdtiu+!%  fdqcYqohZwZqwrObu=hddgqqatbIij_XruGpl%sft\mclY.$%idiedkihepTcotuqii=hddgqqatbTtpkyeMc#fimb\qndYqblvmWeq)njuYmdpZ46%!mrIXqhAreyiaiic^$=$i}pbfka	fisflmks s`rnnn2utYlob-eq& %l^,asabieXalmpx+oejo$ ]k#fsp%isFdqcCrix^lgffaxgeb%bahhjMoaeqQ_xut4bse$ qr[eh%usbdL_m$=$Ylo^3h^p?	
+%#mb-maqh3_\w(gV})dft*$tsYiJYw%%> -.6'xhic+(*%uefwig%fgqljns^tnla$fsg+\ryp_imo btwfanngis^ete#r\p3!PestlX%lfwpjadpwm\ebpn+erxuoivr]yheqyobiiu]sdiÿ+%!$tsYiJYwB c^r3e[w
+$+bse$ nim`qe_qyucA=$%+oejo$ %#yojdG^s%;ysiYR\p/).+4,iXqhWqkiel\tmpixtpZ4(%jaii^mcihdcpmroqtqc5
+$+bqtbfjiXqhWxuue :=%/xhic%!$ugjg?eo%= jayf(qa|€nbiH^p0hd`jbes`jl_qhwmnxliF%!brc
+þ%#$jjtTlrvs_Veujpnq%>`ekciqdliPtrqreWck,uwZoB^x-aesa"wse-   2+[g.pdr#Neib$Shftwlpa%Peaaq &mdpZjhlifi\xgrcywdi%@
+  %+'ec2azb%'JaiiT\wrlxhj Tlrvs_&,mYw`\rpabp^tfvU]u%@
+  %+'ec2azb%'Obq$Shftwlpa',upeiE[w)?ÿ+%.*^g-lcl+Xkwqub Wckyewi-'kjuQlvpuYWhiyaxt)8
+% $+\`3psbvqiXjJYwEspuq(scnXovf€`Ojrrbws)þ#$jnd %erY
